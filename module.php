1161d4cc8429d72166045fb1783d690f4358a050
Dominique Gijzen <dominique@gijzendma.nl>
2020-05-04 20:01:00 +0200

    First stuffy

---
+++ module.php
@@ -0,0 +1,14 @@
+<?php
+    // Klassendefinition
+    class IPSymconHeosdev extends IPSModule {
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MuteHeos() {
+            echo $this->InstanceID;
+        }
+    }
